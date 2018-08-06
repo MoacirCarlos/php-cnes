14ae7490dcffe8c01fe853b73a92f595795a4011
Moacir Carlos Kurmann <moacir@mck2.com.br>
2018-08-06 11:21:39 -0300

    Consumo do serviço com informações dos profissionais da unidade de saúde.

---
+++ estabelecimento-profissionais.php
@@ -0,0 +1,48 @@
+<?php
+
+error_reporting(E_ALL); 
+ini_set("display_errors", 1); 
+
+include "ws-security.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Profissionais vinculados a unidade de saúde</title>
+</head>
+<body>
+
+<?php
+
+$options = array( 'location' => 'https://servicoshm.saude.gov.br/cnes/ProfissionalSaudeService/v1r0', 
+                  'encoding' => 'utf-8', 
+                  'soap_version' => SOAP_1_2,
+                  'connection_timeout' => 180,
+                  'trace'        => 1, 
+                  'exceptions'   => 1 );
+
+$client = new SoapClient('https://servicoshm.saude.gov.br/cnes/ProfissionalSaudeService/v1r0?wsdl', $options);   
+$client->__setSoapHeaders(soapClientWSSecurityHeader('CNES.PUBLICO', 'cnes#2015public'));
+
+$function = 'consultarProfissionaisSaude';
+
+$arguments= array( 'prof' => array(
+						'FiltroPesquisaEstabelecimentoSaude' => array(
+								'CodigoCNES' => array(
+	                            'codigo'     => '2530031'
+	                        )
+						)
+                    )
+                );
+
+$result = $client->__soapCall($function, $arguments);
+
+print("<pre>".print_r($result,true)."</pre>");
+
+?>
+	
+</body>
+</html>
